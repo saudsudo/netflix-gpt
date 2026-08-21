999362b24cdf98272bb74ea3c6d5591f5bca4a11
Saud Siddiqui <siddiqui@Sauds-MacBook-Pro.local>
2026-08-21 18:24:12 +0530

    commit

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
-
-const MainContainer = ({title, overview}) => {
-   
+const MainContainer = ({ title, overview }) => {
   return (
-    <div className='flex flex-col m-20 w-5/12 bg-gray-400 text-white font-semibold'>
-        <h1 className='text-3xl'> Name of the Movie {title}</h1>
-        <p>Descrption of the movie {overview}</p>
-
+    <div>
+      <div className="relative w-10/12 mt-40 bg-yellow-300">
+        Movie Trailer
+        <div className=" absolute m-20 w-5/12 bg-gray-400 text-white font-semibold rounded-lg mt-60">
+          <h1 className="text-3xl p-2"> {title}</h1>
+          <p className="p-2"> {overview}</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default MainContainer;
